760dda3b228c103440f61a6eb3d00bb2240f4a8a
dxf <duxufeng@qq.com>
2026-08-18 09:06:04 +0800

    重新提交插件调用方式

--- bin/entrix.bat
+++ bin/entrix.bat
@@ -1,20 +1,4 @@
 @echo off
 REM Entrix CLI launcher script for Windows
-REM This script finds and executes the entrix Python module
 
-where entrix >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    entrix %*
-    exit /b %ERRORLEVEL%
-)
-
-REM Fallback to python -m entrix
-where python >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    python -m entrix %*
-    exit /b %ERRORLEVEL%
-)
-
-echo Error: Neither 'entrix' command nor Python found >&2
-echo Please install entrix: pip install entrix >&2
-exit /b 1
+python -m entrix %*
